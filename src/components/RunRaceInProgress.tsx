9281a56e0e9a130b516506bae2b0823d6d953989
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2024-11-26 21:33:22 +0000

    Initial visualisation of mixed r1

--- src/components/RunRaceInProgress.tsx
+++ src/components/RunRaceInProgress.tsx
@@ -1,9 +1,16 @@
+import { For } from "solid-js";
 import { Round } from "../api/krm";
+import { raceConfig } from "../kings";
 
 export default function RunRaceInProgress(props: { round: Round }) {
+  const mixed = () => props.round.teams['mixed']
   return (
     <>
       {props.round.date} {props.round.league}
+      <For each={mixed()}>{(team) => {
+        return <div>{team}</div>
+      }}</For>
+      <pre>{JSON.stringify(raceConfig[mixed().length], null, 2)}</pre>
     </>
   )
 }
