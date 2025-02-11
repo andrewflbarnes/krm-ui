182c1b044db51b5637696a72fd0c0ce83b338640
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2025-02-11 19:36:35 +0000

    Add netlify build script

---
+++ netlify-build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+pnpm install -g corepack@latest
+corepack enable
+pnpm install
+pnpm pkg set version="$(pnpm pkg get version | awk -F'"' '{print $2}')-$(git rev-parse --short HEAD)"
+pnpm build
+cp dist/krm-ui/index.html dist/krm-ui/404.html
