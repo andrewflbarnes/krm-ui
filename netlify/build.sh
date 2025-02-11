d81b24c19b870ce61db9e427a5a767eb21ad12a3
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2025-02-11 20:29:34 +0000

    Maybe these redirect rules will work properly ...

--- netlify/build.sh
+++ netlify/build.sh
@@ -5,5 +5,4 @@ corepack enable
 pnpm install
 pnpm pkg set version="$(pnpm pkg get version | awk -F'"' '{print $2}')-$(git rev-parse --short HEAD)"
 pnpm build
-cp dist/krm-ui/index.html dist/krm-ui/404.html
 cp netlify/_redirects dist/_redirects
