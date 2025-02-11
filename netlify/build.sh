e8145ee8d979c8331ca3332e8a58693972c690af
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2025-02-11 19:53:29 +0000

    Fix netlify build with redirects

--- netlify/build.sh
+++ netlify/build.sh
@@ -6,3 +6,4 @@ pnpm install
 pnpm pkg set version="$(pnpm pkg get version | awk -F'"' '{print $2}')-$(git rev-parse --short HEAD)"
 pnpm build
 cp dist/krm-ui/index.html dist/krm-ui/404.html
+cp netlify/_redirects dist/_redirects
