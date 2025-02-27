61c5b268637c14633eac3fbf83d2b7311419ddb4
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2025-02-27 20:52:55 +0000

    Move auth logic to a separate context/provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,9 @@ import { QueryClient, QueryClientProvider } from "@tanstack/solid-query";
 import { BreadcrumberProvider } from "./hooks/breadcrumb";
 import { KingsProvider } from "./kings";
 import { Route, Router } from "@solidjs/router";
-import { createComputed, createContext, createMemo, createSignal, lazy, ParentProps, useContext } from "solid-js";
+import { createMemo, createSignal, lazy, ParentProps } from "solid-js";
 import { createPalette, createTheme, ThemeProvider } from "@suid/material";
 import AppLayout from "./AppLayout";
-import { ClerkProvider, useAuth as useClerkAuth } from "clerk-solidjs";
-import { dark } from "@clerk/themes";
-import { signInWithCustomToken, signOut, User } from "firebase/auth";
 const Home = lazy(() => import("./pages/Home"));
 const Developer = lazy(() => import("./pages/Developer"));
 const RaceManager = lazy(() => import("./pages/Manage"));
@@ -19,11 +16,10 @@ const RaceManagerConfigure = lazy(() => import("./pages/ManageConfigure"));
 const RaceManagerContinue = lazy(() => import("./pages/ManageContinue"));
 const RaceManagerNew = lazy(() => import("./pages/ManageNew"));
 const RunRace = lazy(() => import("./pages/RunRace"));
-import { auth } from "./firebase";
 import { Toaster } from "solid-toast";
+import { AuthProvider } from "./hooks/auth";
 
 const queryClient = new QueryClient()
-const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 export default function App() {
   return (
@@ -80,67 +76,18 @@ function HydratedAppLayout(props: ParentProps) {
     return newMode
   })
   return (
-    <ClerkProvider publishableKey={clerkPubKey} appearance={{ baseTheme: dark }}>
-      <AuthProvider>
-        <QueryClientProvider client={queryClient}>
-          <BreadcrumberProvider>
-            <KingsProvider>
-              <ThemeProvider theme={theme}>
-                <AppLayout onModeChange={handleModeChange}>
-                  {props.children}
-                </AppLayout>
-              </ThemeProvider>
-            </KingsProvider>
-          </BreadcrumberProvider>
-        </QueryClientProvider>
-      </AuthProvider>
-    </ClerkProvider>
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <BreadcrumberProvider>
+          <KingsProvider>
+            <ThemeProvider theme={theme}>
+              <AppLayout onModeChange={handleModeChange}>
+                {props.children}
+              </AppLayout>
+            </ThemeProvider>
+          </KingsProvider>
+        </BreadcrumberProvider>
+      </QueryClientProvider>
+    </AuthProvider>
   )
-}
-
-function AuthProvider(props: ParentProps) {
-  const { userId, getToken } = useClerkAuth()
-  const [fbUser, setFbUser] = createSignal<User>()
-  const [authLock, setAuthLock] = createSignal(false)
-
-  createComputed(() => {
-    if (!userId()) {
-      setFbUser(undefined)
-      signOut(auth)
-      setAuthLock(false)
-    }
-    if (userId() && !fbUser() && !authLock()) {
-      setAuthLock(true)
-      const authFb = async () => {
-        if (!authLock()) {
-          return
-        }
-        let error = false
-        while (true) {
-          try {
-            if (!userId()) {
-              return
-            }
-            const t = await getToken({ template: "integration_firebase" })
-            const { user } = await signInWithCustomToken(auth, t || '')
-            setFbUser(user)
-            if (error) {
-              notification.success("Authenticated with backend")
-            }
-            setAuthLock(false)
-            return
-          } catch (e) {
-            error = true
-            console.error(e)
-            notification.error("Failed to authenticate with backend")
-            // TODO set backend failure signal and display
-            await new Promise(r => setTimeout(r, 10000))
-          }
-        }
-      }
-      authFb()
-    }
-  })
-
-  return <>{props.children}</>
 }
