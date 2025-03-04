5937e871d6df4cf539cd9181c9a1f8efca1f2520
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2025-03-04 10:37:20 +0000

    Fix build script - we no longer use a redirects file

--- netlify/build.sh
+++ netlify/build.sh
@@ -12,4 +12,3 @@ fi
 pnpm pkg set version="$(pnpm pkg get version | awk -F'"' '{print $2}')-${branch:+$branch-}$(git rev-parse --short HEAD)"
 pnpm pkg get version
 pnpm build
-cp netlify/_redirects dist/_redirects
