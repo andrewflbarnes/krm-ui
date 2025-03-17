57961b1506d63fb50ff1262c89d70a86577060ca
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2025-03-17 01:33:51 +0000

    Add link back to race stage when incorrect nav to abandon

--- src/pages/RunRaceAbandoned.tsx
+++ src/pages/RunRaceAbandoned.tsx
@@ -1,12 +1,17 @@
+import { A } from "@solidjs/router";
 import { Typography } from "@suid/material";
+import { Show } from "solid-js";
 import { useRaceOptions } from "../hooks/results";
+import { Round } from "../kings";
 
 export default function RunRaceAbandoned() {
   const { useRound } = useRaceOptions()
   const query = useRound()
   const round = () => query.data
   return (
-    <>
+    <Show when={round().status == "abandoned"} fallback={
+      <GoToRace round={round()} />
+    }>
       <Typography>
         Races were abandoned for this {round().league} round.
       </Typography>
@@ -16,6 +21,22 @@ export default function RunRaceAbandoned() {
       <Typography>
         {round().description}
       </Typography>
-    </>
+    </Show>
+  )
+}
+
+function GoToRace(props: {
+  round: Round;
+}) {
+  return (
+    <div>
+      This round hasn't been abandoned,
+      &nbsp;
+      <A href={`/${props.round.id}/${props.round.status}`} style={{ "text-decoration": "none" }}>
+        <Typography color="primary" style={{ display: "inline" }}>
+          view the latest stage here
+        </Typography>
+      </A>
+    </div>
   )
 }
