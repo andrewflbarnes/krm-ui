ff2e9a4d9bb62dfc0848ce9116c9993a98db65b0
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2025-03-28 01:23:03 +0000

    Add knockout and results top config, restrict visible seeds

--- src/pages/CreateRoundConfig.tsx
+++ src/pages/CreateRoundConfig.tsx
@@ -1,26 +1,67 @@
 import { Button } from "@suid/material";
 import { createSignal, Show } from "solid-js";
 import CustomRoundStage from "../components/CustomRoundStage";
-import { RaceStage } from "../kings";
+import { ConfigState } from "../hooks/create-config";
+import { isStage } from "../kings/utils";
+
+const conf: {
+  [state in ConfigState]: {
+    prev?: ConfigState;
+    next?: ConfigState;
+  }
+} = {
+  stage1: {
+    next: "stage2",
+  },
+  stage2: {
+    prev: "stage1",
+    next: "knockout",
+  },
+  knockout: {
+    prev: "stage2",
+    next: "results",
+  },
+  results: {
+    prev: "knockout",
+  }
+}
 
 export default function CreateRoundConfig() {
-  const [stage, setStage] = createSignal<RaceStage>("stage1")
-  const handleStageChange = () => {
-    const current = stage()
-    const next = current == "stage1" ? "stage2" : "stage1"
+  const [stage, setStage] = createSignal<ConfigState>("stage1")
+  const handleStageChange = (next: ConfigState) => {
     setStage(next)
   }
+  const prev = () => conf[stage()].prev
+  const next = () => conf[stage()].next
   return (
     <div style={{ display: "flex", "align-items": "center", "flex-direction": "column" }}>
-      <Button onClick={handleStageChange}>
-        {stage() == "stage1" ? "Stage 2 >>>" : "<<< Stage 1"}
-      </Button>
+      <div style={{
+        display: "grid",
+        "align-items": "center",
+        "grid-template-columns": "1fr 1fr",
+        width: "100%",
+      }}>
+        <Button
+          disabled={!prev()}
+          onClick={() => handleStageChange(prev())}
+        >
+          &lt;&lt;&lt;&nbsp;{prev()}
+        </Button>
+        <Button
+          disabled={!next()}
+          onClick={() => handleStageChange(next())}
+        >
+          {next()}&nbsp;&gt;&gt;&gt;
+        </Button>
+      </div>
       <div style={{ margin: "0 auto" }}>
-        <Show when={stage()} keyed>
+        <Show when={isStage(stage())} keyed fallback={(
+          <>TODO</>
+        )}>{(s) => (
           <CustomRoundStage
-            stage={stage()}
+            stage={s}
           />
-        </Show>
+        )}</Show>
       </div>
     </div>
   )
