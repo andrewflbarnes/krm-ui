13d84a454589ebabffa3391d03d0fc5c1818bee3
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2025-04-18 13:26:09 +0100

    Add config for 10 teams

--- src/kings/config.ts
+++ src/kings/config.ts
@@ -398,6 +398,96 @@ export const raceConfig: {
       ]),
     ]
   },
+  10: {
+    stage1: [
+      {
+        name: "A",
+        seeds: wrapStage1Seeds([0, 5, 6, 9]),
+        template: miniLeagueTemplates.mini4,
+      },
+      {
+        name: "B",
+        seeds: wrapStage1Seeds([1, 4, 7]),
+        template: miniLeagueTemplates.mini3,
+      },
+      {
+        name: "C",
+        seeds: wrapStage1Seeds([2, 3, 8]),
+        template: miniLeagueTemplates.mini3,
+      },
+    ],
+    stage2: [
+      {
+        name: "I",
+        seeds: [
+          { group: "A", position: 0 },
+          { group: "B", position: 1 },
+          { group: "C", position: 0 },
+          { group: "A", position: 2 },
+        ],
+        template: miniLeagueTemplates.mini4,
+      },
+      {
+        name: "II",
+        seeds: [
+          { group: "A", position: 1 },
+          { group: "B", position: 0 },
+          { group: "C", position: 1 },
+        ],
+        template: miniLeagueTemplates.mini3,
+      },
+      {
+        name: "III",
+        seeds: [
+          { group: "A", position: 3 },
+          { group: "B", position: 2 },
+          { group: "C", position: 2 },
+        ],
+        template: miniLeagueTemplates.mini3,
+      },
+    ],
+    knockout: [
+      {
+        name: "7th/8th",
+        seeds: [
+          { group: "I", position: 3 },
+          { group: "III", position: 0 },
+        ],
+        template: miniLeagueTemplates.knockout,
+      },
+      {
+        name: "5th/6th",
+        seeds: [
+          { group: "I", position: 2 },
+          { group: "II", position: 2 },
+        ],
+        template: miniLeagueTemplates.knockout,
+      },
+      {
+        name: "3rd/4th",
+        seeds: [
+          { group: "I", position: 1 },
+          { group: "II", position: 1 },
+        ],
+        template: miniLeagueTemplates.knockout,
+      },
+      {
+        name: "1st/2nd",
+        seeds: [
+          { group: "I", position: 0 },
+          { group: "II", position: 0 },
+        ],
+        template: miniLeagueTemplates.knockout,
+      },
+    ],
+    results: [
+      ...resultsFromKnockout(8),
+      ...resultsForGroup("stage2", "III", [
+        { position: 1, rank: 9 },
+        { position: 2, rank: 10 },
+      ]),
+    ]
+  },
 } as const
 // Round config tests
 // - stage 2 teams <= stage 1 teams
