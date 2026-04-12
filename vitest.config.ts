05fbfec606088c40e60528a4b064707f3a6f174f
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2026-04-12 14:06:58 +0100

    wip

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,11 @@
 import { configDefaults, defineConfig, mergeConfig } from 'vitest/config';
 import viteConfig from './vite.config.js';
+//import path from 'node:path';
+//import { fileURLToPath } from 'node:url';
+//import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+//const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default mergeConfig(viteConfig, defineConfig({
   test: {
     coverage: {
@@ -15,6 +20,26 @@ export default mergeConfig(viteConfig, defineConfig({
           environment: 'jsdom'
         }
       },
+      //{
+      //  extends: true,
+      //  plugins: [
+      //    // The plugin will run tests for the stories defined in your Storybook config
+      //    // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+      //    storybookTest({
+      //      configDir: path.join(dirname, '.storybook')
+      //    })],
+      //  test: {
+      //    name: 'storybook',
+      //    browser: {
+      //      enabled: true,
+      //      headless: true,
+      //      provider: 'playwright',
+      //      instances: [{
+      //        browser: 'chromium'
+      //      }]
+      //    }
+      //  }
+      //}
     ]
   }
 }));
