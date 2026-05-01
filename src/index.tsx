1d20e000cf94553d2030cff5c9d3846367d1e434
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2026-05-01 02:30:30 +0100

    chore: add robots.txt and consolidate index.css files

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
 import App from "./App";
-import "./index.css";
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
